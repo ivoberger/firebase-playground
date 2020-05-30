7eb8cd3787f57179e9c000472cd655d33eb638cd
Ivo Berger <ivo@ivoberger.com>
2020-05-30 15:05:16 +0200

    intial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,11 @@
+import * as functions from "firebase-functions";
+import fetch from "node-fetch";
+
+const nextcloudWebcron = functions.pubsub
+  .schedule("every 5 minutes")
+  .onRun(async (context) => {
+    const response = await fetch("https://cloud.ivoberger.com/cron.php");
+    console.info(response);
+  });
+
+export { nextcloudWebcron };
